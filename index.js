06dbb8564c68a4141c9985b05cc4dabec55cb078
cpan1995 <cpan1995@gmail.com>
2021-06-25 21:12:25 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num){
+  let result;
+  if (num <= 400){
+    result = 'This one is on me!';
+  }
+  else if(num > 2000 && num < 2500){
+    result = "I will gladly take your thirty bucks.";
+  }
+  else if (num > 2500){
+    result = "No can do.";
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let result;
+  if (city == 'NYC'){
+    result = 'Ok, sounds good.';
+  }
+  else if (city != 'NYC'){
+    result = "No go."
+  }
+  return result;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(someString){
   // Write your code here!
+  let result;
+  if (someString == 'generous'){
+    result = 'Thank you so much.';
+  }
+  else if (someString == 'not as generous'){
+    result = 'Thank you.';
+  }
+  else{
+    result = 'Bye.';
+  }
+  return result;
 }
